78ac3af32fec50563f0726f133676703256dac9a
Yizhou Wang <81718264+yiw008@users.noreply.github.com>
2021-11-06 23:18:35 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,12 +3,44 @@
 
 const CACHE_NAME = 'lab-7-starter';
 
+var urlsToCache = [
+  '/',
+  'assets/styles/main.css',
+  'assets/scripts/main.js',
+  'assets/scripts/Router.js',
+  'assets/images/icons/0-star.svg',
+  'assets/images/icons/1-star.svg',
+  'assets/images/icons/2-star.svg',
+  'assets/images/icons/3-star.svg',
+  'assets/images/icons/4-star.svg',
+  'assets/images/icons/5-star.svg',
+  'assets/images/icons/arrow-down.png',
+  'assets/components/RecipeCard.js',
+  'assets/components/RecipeExpand.js',
+  'https://introweb.tech/assets/json/ghostCookies.json',
+  'https://introweb.tech/assets/json/birthdayCake.json',
+  'https://introweb.tech/assets/json/chocolateChip.json',
+  'https://introweb.tech/assets/json/stuffing.json',
+  'https://introweb.tech/assets/json/turkey.json',
+  'https://introweb.tech/assets/json/pumpkinPie.json'
+];
+
 // Once the service worker has been installed, feed it some initial URLs to cache
 self.addEventListener('install', function (event) {
   /**
    * TODO - Part 2 Step 2
    * Create a function as outlined above
    */
+
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+
 });
 
 /**
@@ -21,6 +53,8 @@ self.addEventListener('activate', function (event) {
    * TODO - Part 2 Step 3
    * Create a function as outlined above, it should be one line
    */
+
+  event.waitUntil(clients.claim());
 });
 
 // Intercept fetch requests and store them in the cache
@@ -29,4 +63,42 @@ self.addEventListener('fetch', function (event) {
    * TODO - Part 2 Step 4
    * Create a function as outlined above
    */
+
+   event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // IMPORTANT:Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response.
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT:Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
 });
