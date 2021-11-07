206091c58dc5f7469b2a4b87d9f5f694dd4d356a
Yizhou Wang <81718264+yiw008@users.noreply.github.com>
2021-11-06 23:17:59 -0700

    Update Router.js

--- assets/scripts/Router.js
+++ assets/scripts/Router.js
@@ -38,6 +38,8 @@ export class Router {
      * router instance using the 'this' keyword. Substitute 'home' for the variable
      * page
      */
+
+     this[page] = pageFunc;
   }
 
   /**
@@ -65,5 +67,23 @@ export class Router {
      *     and URL + hash to history
      *  4. Finally, call the stored function for the given page
      */
+
+    if (!(this[page])){
+      console.log('Error');
+      return;
+    }
+
+    var hash;
+    if (page == 'home'){
+      hash = '';
+    } else {
+      hash = "#" + page;
+    }
+
+    if (!statePopped && window.location.hash != hash){
+      history.pushState({page}, '', window.location.origin + hash);
+    }
+
+    this[page]();
   }
 }
